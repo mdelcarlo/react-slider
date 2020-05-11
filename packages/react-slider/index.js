0df594046e4f21ebfc60ea4e74325a679ee38e1a
Matias Del Carlo <mati.dc89@gmail.com>
2020-05-12 08:21:21 +1200

    Add drag and drop

--- packages/react-slider/index.js
+++ packages/react-slider/index.js
@@ -10,6 +10,7 @@ function Thumb(props) {
       css={{
         left: `calc(${props.position}% - 8px)`,
       }}
+      {...props}
     />
   );
 }
@@ -18,31 +19,54 @@ function Slider({ value, min = 0, max = 100, step = 1 }) {
   const [selectedValue, setSelectedValue] = useState('');
   const [values, setValues] = useState([]);
 
-  function getArrayOfValues({ max, min, step }) {
+  const getArrayOfValues = ({ max, min, step }) => {
     const count = (max - min) / step;
     const arrayValues = Array(count + 1)
       .fill()
       .map((_, i) => min + i * step);
     return arrayValues;
-  }
+  };
 
-  function getPercentualValuePosition(value, values) {
+  const getPercentualValuePosition = (value, values) => {
     const valueArrayPosition = values.indexOf(value);
     if (valueArrayPosition === 0) return 0;
     return (valueArrayPosition / (values.length - 1)) * 100;
-  }
+  };
 
-  function handleSliderClick(event) {
+  const handleSliderClick = event => {
     const targetRect = event.target.getClientRects()[0];
     const relativePosition = (event.pageX - targetRect.x) / targetRect.width;
     var position = Math.round((values.length - 1) * relativePosition);
     moveThumbPosition(position);
-  }
+  };
 
-  function moveThumbPosition(position) {
+  const moveThumbPosition = position => {
     console.log(position);
     setSelectedValue(values[position]);
-  }
+  };
+
+  const handleThumbMouseDown = event => {
+    const targetRect = event.target.parentNode.getClientRects()[0];
+
+    document.addEventListener('mousemove', handleThumbMove(targetRect));
+    document.addEventListener(
+      'mouseup',
+      handleThumbMouseUp(handleThumbMove(targetRect), handleThumbMouseUp)
+    );
+  };
+
+  const handleThumbMove = targetRect => event => {
+    const relativePosition = (event.pageX - targetRect.x) / targetRect.width;
+    if (relativePosition < 0) relativePosition = 0;
+    if (relativePosition > 1) relativePosition = 1;
+    var position = Math.round((values.length - 1) * relativePosition);
+    moveThumbPosition(position);
+  };
+
+  const handleThumbMouseUp = (handleThumbMove, handleThumbMouseUp) => event => {
+    document.removeEventListener('mousemove', handleThumbMove);
+    document.removeEventListener('mouseup', handleThumbMouseUp);
+  };
 
   useEffect(() => {
     console.log('effect');
@@ -52,7 +76,10 @@ function Slider({ value, min = 0, max = 100, step = 1 }) {
 
   return (
     <div className="slider" onClick={handleSliderClick}>
-      <Thumb position={getPercentualValuePosition(selectedValue, values)} />
+      <Thumb
+        onMouseDown={handleThumbMouseDown}
+        position={getPercentualValuePosition(selectedValue, values)}
+      />
     </div>
   );
 }
